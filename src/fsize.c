8049519cc144b9b984d1f4f308bf2aabce9caf2b
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-07-24 10:13:22 +0530

    added top comment

--- src/fsize.c
+++ src/fsize.c
@@ -1,3 +1,9 @@
+/**
+ * @file fsize.c
+ * @license This file is licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007. You may obtain a copy of this license at https://www.gnu.org/licenses/gpl-3.0.en.html.
+ * @author Tushar Chaurasia (Dark-CodeX)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
